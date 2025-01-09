c2219053da308901e0a3c85c7e33dde67f0eb8ab
webwithsumit <sumit2410kushwaha@gmail.com>
2025-01-09 13:28:12 +0530

    about page

---
+++ src/pages/About/About.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <>
+            <div className='bg-gray-500 w-full'>This is About section</div>
+        </>
+    )
+}
+
+export default About;
