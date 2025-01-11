e296ae8e07d28bfd1b8a51daa3b526c57de9cfde
webwithsumit <sumit2410kushwaha@gmail.com>
2025-01-11 12:13:12 +0530

    profile page

---
+++ src/pages/Profile/Profile.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Profile = () => {
+    return (
+        <>
+            <div className='bg-gray-500 w-full'>This is you profile</div>
+        </>
+    )
+}
+
+export default Profile;
